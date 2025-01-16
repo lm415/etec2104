8147d18151ac3a4d6718adc21e888c99e8e5719f
lm415 <mcguireluke04@gmail.com>
2025-01-16 14:19:01 -0500

    first commit

---
+++ .vscode/foo.py
@@ -0,0 +1,4 @@
+
+import sys
+import subprocess
+subprocess.call([sys.executable, "-m","pip","install","--user","ws4py"])
---
+++ src/main.py
@@ -0,0 +1,18 @@
+import cherrypy
+import mako.template
+import os.path
+
+
+PYPATH = os.path.dirname(__file__)
+
+class App:
+    @cherrypy.expose
+    def index(self):
+        return "I like pie!"
+
+    @cherrypy.expose
+    def test(self):
+        t = mako.template.Template(filename=f"{PYPATH}/../html/test.html")
+        return t.render(foobar=42)
+app = App()
+cherrypy.quickstart(app)
